795c83be7e0319ce6326422ec4f3f78f307e6f6a
kevin <kqa5573@psu.edu>
2026-02-05 11:10:26 -0500

    Added names for monsters

--- src/monsters/Main.java
+++ src/monsters/Main.java
@@ -5,5 +5,9 @@ public class Main {
     public static void main(String[] args){
         Monsters c1 = new Grass("Chikorita");
         Monsters c2 = new Fire("Charizard");
+
+        System.out.println("Monster 1 name: " + c1.getName());
+        System.out.println("Monster 1 name: " + c2.getName());
+
     }
 }
